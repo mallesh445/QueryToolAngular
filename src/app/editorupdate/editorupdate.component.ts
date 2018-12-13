321454f294ec49fb9e654be45d2d6360822d4e1e
Mallesh.Machukuri <mallesh.machukuri@ggktech.local>
2018-12-13 17:53:13 +0530

    Query title parameter code added in insert query function

--- src/app/editorupdate/editorupdate.component.ts
+++ src/app/editorupdate/editorupdate.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder,FormArray, Validators, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder, FormArray, Validators, FormControl } from '@angular/forms';
 import { ShipbobService } from '../services/shipbob.service';
-import { Routes, Router,ActivatedRoute } from '@angular/router';
-import {SessionService} from '../services/session.services';
+import { Routes, Router, ActivatedRoute } from '@angular/router';
+import { SessionService } from '../services/session.services';
 
 @Component({
   selector: 'app-editorupdate',
@@ -10,107 +10,110 @@ import {SessionService} from '../services/session.services';
   styleUrls: ['./editorupdate.component.scss']
 })
 export class EditorupdateComponent implements OnInit {
-  form:FormGroup;
-  errorMessage:any;
-  ddl:any;
-  editShow:boolean=true;
-  columns:any;
-  builtQuery:string;
-  selectedModule:string = "";
-  requiredInsertData:string;
-  selectedOperation:string;
-  parametersArray:Array<any>[] = [];
-  result:any;
-  allModules:any;
+  form: FormGroup;
+  errorMessage: any;
+  ddl: any;
+  editShow: boolean = true;
+  columns: any;
+  builtQuery: string;
+  queryTitle: string;
+  selectedModule: string = "";
+  requiredInsertData: string;
+  selectedOperation: string;
+  parametersArray: Array<any>[] = [];
+  result: any;
+  allModules: any;
   queryId: number = 0;
-  
-    constructor(private fb: FormBuilder, private service: ShipbobService, private router:Router,private route: ActivatedRoute, private mySession:SessionService) { }
-  
-    insertQuery(){
 
-      for (let item of this.form.value.items) {
-        this.parametersArray.push(item);
-     }
+  constructor(private fb: FormBuilder, private service: ShipbobService, private router: Router, private route: ActivatedRoute, private mySession: SessionService) { }
 
-      console.log(JSON.stringify(this.parametersArray));
+  insertQuery() {
 
-      console.log(this.selectedModule+this.selectedOperation);
-      this.requiredInsertData=this.selectedModule+","+this.selectedOperation+","+this.builtQuery+","+this.parametersArray;
-      console.log("requiredInsertData:  "+ this.requiredInsertData);
-      this.service.insertNewQueryInScripts(this.requiredInsertData).subscribe(
-        resq=>{ 
-          console.log(resq);
-          this.result=resq;
-        },
-        
-        (error) => {
-          console.log(error);
-          this.errorMessage = error;
-        },
-        (complete) => {
-          this.router.navigateByUrl("modulelist/"+ this.selectedModule);
-        }
-      );
+    for (let item of this.form.value.items) {
+      this.parametersArray.push(item);
     }
-    formSubmit(){
-      console.log(this.selectedModule);
-      this.requiredInsertData=this.selectedModule+","+this.selectedOperation+","+this.builtQuery;
-      console.log(this.requiredInsertData);
-      const p = {  ...this.form.value };
-      console.log(p);
-    }
+
+    console.log(JSON.stringify(this.parametersArray));
+
+    console.log(this.selectedModule + this.selectedOperation);
+    this.requiredInsertData = this.selectedModule + "," + this.selectedOperation + "," + this.builtQuery +","+this.queryTitle+ "," + this.parametersArray;
+    console.log("RequiredInsertData:  " + this.requiredInsertData);
+    this.service.insertNewQueryInScripts(this.requiredInsertData).subscribe(
+      resq => {
+        console.log(resq);
+        this.result = resq;
+      },
+
+      (error) => {
+        console.log(error);
+        this.errorMessage = error;
+      },
+      (complete) => {
+        this.router.navigateByUrl("modulelist/" + this.selectedModule);
+      }
+    );
+  }
+
+
+  formSubmit() {
+    console.log(this.selectedModule);
+    this.requiredInsertData = this.selectedModule + "," + this.selectedOperation + "," + this.builtQuery+","+this.queryTitle;
+    console.log(this.requiredInsertData);
+    const p = { ...this.form.value };
+    console.log(p);
+  }
   ngOnInit() {
-    this.route.params.subscribe(params=>{
-      this.queryId=params['id']      
+    this.route.params.subscribe(params => {
+      this.queryId = params['id']
     });
 
 
-    var editQuery = this.mySession.session;    
+    var editQuery = this.mySession.session;
     console.log(editQuery);
-    
+
 
     this.createForm();
-    this.service.getAllModules().subscribe(resModule=>{
-      this.allModules=resModule;
+    this.service.getAllModules().subscribe(resModule => {
+      this.allModules = resModule;
     },
-    (error) => {
-      console.log(error);
-      this.errorMessage = error;
-    },
-    () => {      
-    this.InitializeForm(editQuery);
-    }
+      (error) => {
+        console.log(error);
+        this.errorMessage = error;
+      },
+      () => {
+        this.InitializeForm(editQuery);
+      }
     );
   }
 
   createForm() {
     this.form = this.fb.group({
-    name: [''],
-    items: this.fb.array([this.createArray()])
-  });
-}
-createArray(): FormControl {
-   return new FormControl();
-}
-
-get items(): FormArray {
- return this.form.get('items') as FormArray;
-}
-addItems() {
-  this.items.push(this.createArray())
-}
-removeItem(index: any) {
-  if(index >= 1) {
-     this.items.removeAt(index);
+      name: [''],
+      items: this.fb.array([this.createArray()])
+    });
   }
-}
+  createArray(): FormControl {
+    return new FormControl();
+  }
 
-InitializeForm(query:object){
-   this.selectedModule = query[0].ModuleId;
-   this.selectedOperation = query[0].OperationId;
-   this.builtQuery = query[0].Script1;
-   
-}
+  get items(): FormArray {
+    return this.form.get('items') as FormArray;
+  }
+  addItems() {
+    this.items.push(this.createArray())
+  }
+  removeItem(index: any) {
+    if (index >= 1) {
+      this.items.removeAt(index);
+    }
+  }
+
+  InitializeForm(query: object) {
+    this.selectedModule = query[0].ModuleId;
+    this.selectedOperation = query[0].OperationId;
+    this.builtQuery = query[0].Script1;
+
+  }
 
 }
 export interface Module {
--- src/app/modulelist/modulescripts.component.ts
+++ src/app/modulelist/modulescripts.component.ts
@@ -87,6 +87,7 @@ para:Array<any>[] = [];
     //console.log(value);
   }
 
+  
   tableToExcel(table, resultGrid){
     let uri = 'data:application/vnd.ms-excel;base64,'
         , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--><meta http-equiv="content-type" content="text/plain; charset=UTF-8"/></head><body><table>{table}</table></body></html>'
