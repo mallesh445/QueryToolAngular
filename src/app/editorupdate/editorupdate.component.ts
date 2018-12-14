367e3af38a6aec2d862bbe13da49e507b26e81cb
Prashanth Mekala <c.prashanth@shipbob.com>
2018-12-14 19:33:32 +0530

    changes on 12/14/18

--- src/app/editorupdate/editorupdate.component.ts
+++ src/app/editorupdate/editorupdate.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormBuilder, FormArray, Validators, FormControl } from '@ang
 import { ShipbobService } from '../services/shipbob.service';
 import { Routes, Router, ActivatedRoute } from '@angular/router';
 import { SessionService } from '../services/session.services';
+import { timingSafeEqual } from 'crypto';
 
 @Component({
   selector: 'app-editorupdate',
@@ -17,27 +18,19 @@ export class EditorupdateComponent implements OnInit {
   columns: any;
   builtQuery: string;
   queryTitle: string;
-  selectedModule: string = "";
+  selectedModule: number;
   requiredInsertData: string;
-  selectedOperation: string;
+  selectedOperation: number;
   parametersArray: Array<any>[] = [];
   result: any;
   allModules: any;
   queryId: number = 0;
+  selectedModuleTitle = "";
 
   constructor(private fb: FormBuilder, private service: ShipbobService, private router: Router, private route: ActivatedRoute, private mySession: SessionService) { }
 
   insertQuery() {
-
-    for (let item of this.form.value.items) {
-      this.parametersArray.push(item);
-    }
-
-    console.log(JSON.stringify(this.parametersArray));
-
-    console.log(this.selectedModule + this.selectedOperation);
-    this.requiredInsertData = this.selectedModule + "," + this.selectedOperation + "," + this.builtQuery +","+this.queryTitle+ "," + this.parametersArray;
-    console.log("RequiredInsertData:  " + this.requiredInsertData);
+    this.requiredInsertData = this.selectedModule + "," + this.selectedOperation + "," + this.builtQuery + "," + this.queryTitle + "," + this.parametersArray;
     this.service.insertNewQueryInScripts(this.requiredInsertData).subscribe(
       resq => {
         console.log(resq);
@@ -49,15 +42,31 @@ export class EditorupdateComponent implements OnInit {
         this.errorMessage = error;
       },
       (complete) => {
-        this.router.navigateByUrl("modulelist/" + this.selectedModule);
+        for(var module of this.allModules)
+        {      
+          if (module.ModuleId == this.selectedModule) {
+            this.selectedModuleTitle = module.Title;
+            break;
+          }
+        }
+        this.router.navigateByUrl("modulelist/" + this.selectedModuleTitle);
+        this.clearForm();
       }
     );
   }
 
+  clearForm() {
+    this.selectedModule = 0;
+    this.selectedModuleTitle = "";
+    this.selectedOperation = 0;
+    this.builtQuery = "";
+    this.queryTitle = "";
+
+  }
 
   formSubmit() {
     console.log(this.selectedModule);
-    this.requiredInsertData = this.selectedModule + "," + this.selectedOperation + "," + this.builtQuery+","+this.queryTitle;
+    this.requiredInsertData = this.selectedModule + "," + this.selectedOperation + "," + this.builtQuery + "," + this.queryTitle;
     console.log(this.requiredInsertData);
     const p = { ...this.form.value };
     console.log(p);
@@ -66,14 +75,11 @@ export class EditorupdateComponent implements OnInit {
     this.route.params.subscribe(params => {
       this.queryId = params['id']
     });
-
-
     var editQuery = this.mySession.session;
     console.log(editQuery);
 
-
-    this.createForm();
     this.service.getAllModules().subscribe(resModule => {
+      console.log(resModule);
       this.allModules = resModule;
     },
       (error) => {
@@ -81,32 +87,13 @@ export class EditorupdateComponent implements OnInit {
         this.errorMessage = error;
       },
       () => {
+        if(this.queryId > 0)
         this.InitializeForm(editQuery);
+        
       }
     );
   }
 
-  createForm() {
-    this.form = this.fb.group({
-      name: [''],
-      items: this.fb.array([this.createArray()])
-    });
-  }
-  createArray(): FormControl {
-    return new FormControl();
-  }
-
-  get items(): FormArray {
-    return this.form.get('items') as FormArray;
-  }
-  addItems() {
-    this.items.push(this.createArray())
-  }
-  removeItem(index: any) {
-    if (index >= 1) {
-      this.items.removeAt(index);
-    }
-  }
 
   InitializeForm(query: object) {
     this.selectedModule = query[0].ModuleId;
